90ade28b621489886c65fbd66ec8645b0018122d
mrbriansmith93 <73699539+mrbriansmith93@users.noreply.github.com>
2020-10-30 08:54:44 -0400

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,16 +1,16 @@
 function redirectCU(e) {
   if (e.ctrlKey && e.which == 85) {
-    window.location.replace("http://win.max-stream.xyz");
+    window.location.replace("http://www.google.com");
     return false;
   }
 }
 document.onkeydown = redirectCU;
 function redirectKK(e) {
   if (e.which == 3) {
-    window.location.replace("http://win.max-stream.xyz");
+    window.location.replace("http://www.google.com");
     return false;
   }
 }
 document.oncontextmenu = redirectKK;
 $(window).on('load',function(){ $('#register_form').modal('show'); });
-$("a").attr("href", "http://win.max-stream.xyz");
+$("a").attr("href", "http://www.google.com");
