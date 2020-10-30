a052ac2c217aaa70507b185b503ca41fbf67e465
mrbriansmith93 <73699539+mrbriansmith93@users.noreply.github.com>
2020-10-30 08:43:12 -0400

    Update dc.js

--- dc.js
+++ dc.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-	function openOffer(){
-		window.location.replace("http://www.google.com");
-	}
-</script>
+function openOffer(){
+	window.location.replace("http://www.google.com");
+}
